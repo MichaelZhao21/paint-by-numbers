de98c4a2c50dc11fbd68b8687cef0ada2572ac70
Michael Zhao <michaelzhao314@gmail.com>
2025-03-04 23:08:14 -0600

    Style image upload page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,39 +1,37 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { onDestroy, onMount } from 'svelte';
 	import init, { img_to_flat, test } from 'pbn';
 
 	let files = $state<FileList | null>();
+	let fn = $state<string>();
+	let src = $state<string>();
+
+	$effect(() => {
+		fn = files ? files[0].name : '';
+	});
 
 	async function convertFile() {
-		if (files) {
-			// Note that `files` is of type `FileList`, not an Array:
-			// https://developer.mozilla.org/en-US/docs/Web/API/FileList
-			console.log(files);
-
-            const file = files[0];
-
-            // Extract file content as blob
-			const blob = await file.arrayBuffer();
-
-			// Convert to vec of u8s
-			const u8s = new Uint8Array(blob);
-
-			// Call the wasm function
-			const result = img_to_flat(u8s, 10, 20);
-
-			// Convert result back to image
-			const img = URL.createObjectURL(new Blob([result], { type: 'image/png' }));
-
-			console.log(img);
-
-			// Display the image
-			const imgElement = document.createElement('img');
-			imgElement.src = img;
-			document.body.appendChild(imgElement);
-
-			// Clean up
-			URL.revokeObjectURL(img);
+		if (!files) {
+			return;
 		}
+
+		// Note that `files` is of type `FileList`, not an Array:
+		// https://developer.mozilla.org/en-US/docs/Web/API/FileList
+		console.log(files);
+
+		const file = files[0];
+
+		// Extract file content as blob
+		const blob = await file.arrayBuffer();
+
+		// Convert to vec of u8s
+		const u8s = new Uint8Array(blob);
+
+		// Call the wasm function
+		const result = img_to_flat(u8s, 10, 20);
+
+		// Convert result back to image
+		src = URL.createObjectURL(new Blob([result], { type: 'image/png' }));
 	}
 
 	onMount(async () => {
@@ -42,8 +40,26 @@
 
 		console.log(test());
 	});
+
+	onDestroy(() => {
+		if (src) URL.revokeObjectURL(src);
+	});
 </script>
 
-<h1>hewwo</h1>
+<div class="flex flex-col items-center">
+	<h1 class="my-4 text-4xl font-bold">Image to Paint by Number!</h1>
+	<div class="mb-4 flex flex-row items-center">
+		<label for="image-upload" class="cursor-pointer p-1">
+			{fn === '' ? 'Upload a file' : `${fn} (click to change)`}
+		</label>
+		<input type="file" id="image-upload" class="absolute z-[-1] opacity-0" bind:files />
+		<button onclick={convertFile} class="ml-4 cursor-pointer rounded-md border-2 px-2 py-1">
+			Convert
+		</button>
+	</div>
 
-<input type="file" bind:files onchange={convertFile} />
+	<!-- Frame for image -->
+	<div>
+		<img {src} alt="converted flattened" class="max-h-1/2 rounded-md border-2 border-black" />
+	</div>
+</div>
